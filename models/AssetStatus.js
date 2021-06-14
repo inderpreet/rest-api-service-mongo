66a4e26751a45baaaffbbf702ed436b51758b0c1
Inderpreet Singh <er.inderpreet@gmail.com>
2021-06-14 12:55:48 -0400

    Add Comments

--- models/AssetStatus.js
+++ models/AssetStatus.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
+// Document Schema for how data is stored in MongoDB
 const schema = mongoose.Schema({
         DockList:[]
 })
 
+// Arguments to model - modelnamestring schemaName, collectionName
 module.exports = mongoose.model("status", schema, "AssetStatus");
