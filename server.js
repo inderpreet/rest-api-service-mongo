f1095b4a1bda6ae1074308de8f6b5dc85ac153d6
Inderpreet Singh <er.inderpreet@gmail.com>
2021-06-15 08:22:12 -0400

    Add dotenv to project. Checks if environment variable exists and if not then read the .env file

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require("./routes")
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
-mongoose.connect("mongodb://localhost:27017/AssetData", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-        .then( ()=>{
-                const app = express();
-                app.use(express.json());
-                app.use("/api", routes);
+if(process.env.FOO == null){
+        require('dotenv').config()
+}
 
-                app.listen(5000, ()=> {
-                        console.log("Server Started");
-                });
-        })
+mongoose
+  .connect("mongodb://localhost:27017/AssetData", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    const app = express();
+    app.use(express.json());
+    app.use("/api", routes);
+
+    app.listen(5000, () => {
+      console.log("Server Started");
+    });
+  });
+
+console.log(process.env.FOO);
